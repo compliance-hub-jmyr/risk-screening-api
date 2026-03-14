23fbaf5efbbed88922f6a1ab54f5583d09a8c503
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2026-03-14 16:10:25 -0500

    feat(IAM): add seeding settings for admin credentials
    Introduce `IamSeedSettings` class for managing seeding configuration and update `appsettings.Development.json` with default admin email and password.

---
+++ RiskScreening.API/Modules/IAM/Infrastructure/Seed/IamSeedSettings.cs
@@ -0,0 +1,7 @@
+namespace RiskScreening.API.Modules.IAM.Infrastructure.Seed;
+
+public class IamSeedSettings
+{
+    public string AdminEmail { get; init; } = "admin@riskscreening.com";
+    public string AdminPassword { get; init; } = "Admin123!";
+}
